61f16ae4bf8502322a06af207474fb07183ae989
Rudrakumar06 <khandrarudra98@gmail.com>
2026-01-29 09:44:02 +0530

    create model and add admin user

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -97,6 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
